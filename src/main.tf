13ee70fb862299618406ac7b3bbaca3bcd9dfc9c
Dhishan Amaranath <iamdhishan@gmail.com>
2025-09-27 18:37:01 -0700

    using rbac for key vault

--- src/main.tf
+++ src/main.tf
@@ -36,26 +36,17 @@ resource "random_password" "vm_admin" {
 }
 
 resource "azurerm_key_vault" "kv" {
-  name                       = local.kv_name
-  location                   = var.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
-  sku_name                   = "standard"
-  purge_protection_enabled   = false
-  soft_delete_retention_days = 7
-  public_network_access_enabled = true
-  enabled_for_disk_encryption = true
-
-  dynamic "access_policy" {
-    for_each = var.key_vault_user_object_ids
-    content {
-      tenant_id = data.azurerm_client_config.current.tenant_id
-      object_id = access_policy.value
-      secret_permissions = ["Get", "List", "Set", "Delete"]
-    }
-  }
-
-  tags = var.tags
+    name                        = local.kv_name
+    location                    = var.location
+    resource_group_name         = azurerm_resource_group.rg.name
+    tenant_id                   = data.azurerm_client_config.current.tenant_id
+    sku_name                    = "standard"
+    purge_protection_enabled    = false
+    soft_delete_retention_days  = 7
+    public_network_access_enabled = true
+    enabled_for_disk_encryption = true
+    enable_rbac_authorization   = true
+    tags                        = var.tags
 }
 
 resource "azurerm_key_vault_secret" "vm_admin_password" {
